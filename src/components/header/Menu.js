410106d9a3a91122bc945d761769dd8a2ff9a313
Yoram Lenaers <yoram.lenaers@student.ap.be>
2021-01-24 21:11:24 +0100

    finalising changes to menu

--- src/components/header/Menu.js
+++ src/components/header/Menu.js
@@ -1,14 +1,19 @@
-import { Link } from "gatsby"
+
 import React from "react"
-import {MenuList} from "./headerStyles/headerStyles"
-
-const Menu = ({menuItems})=>{
-    return <MenuList>
-
-
-
+import { Link } from "gatsby"
+import { MenuList } from "./headerStyles/headerStyles"
+const Menu = ({ menuItems }) => {
+  return (
+    <MenuList>
+      {menuItems.map(({ node: item }, i) => (
+        <li key={i}>
+          <Link activeClassName="nav-active" to={item.path}>
+            {item.label}
+          </Link>
+        </li>
+      ))}
     </MenuList>
-    
+  )
 }
 
 export default Menu
